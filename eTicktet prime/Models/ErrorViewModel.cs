59ee6ad25a4e0342003a8847c2b4c688e8cd30b4
lukasrenstrom <105276560+lukasrenstrom@users.noreply.github.com>
2023-03-02 11:47:26 +0100

    Add project files.

---
+++ eTicktet prime/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicktet_prime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
